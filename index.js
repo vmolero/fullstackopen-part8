bc7f71b6de4d757b87380b812eb6980142bd271b
Victor Molero <victor.molero@gmail.com>
2020-01-04 22:25:36 +0100

    8.4: Books of an author

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, { author }) =>
+      !author ? books : books.filter(book => book.author === author),
     allAuthors: () => authors
   },
   Author: {
